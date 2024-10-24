1494389318e2dcd21d9c576659423c4767c6177a
singhritesh85 <56765895+singhritesh85@users.noreply.github.com>
2024-10-24 20:14:08 +0530

    Delete terraform-ec2-eks-grafana-loki-promtail directory

--- terraform-ec2-eks-grafana-loki-promtail/Grafana-Loki-Promtail-ec2-Applications/grafana-loki-promtail-ec2/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
- backend "s3" {
-   bucket         = "dolo-dempo"
-   key            = "loki-grafana/dev/terraform.tfstate"
-   region         = "us-east-2"
-   encrypt        = true
-   dynamodb_table = "terraform-state"
- }
-}
